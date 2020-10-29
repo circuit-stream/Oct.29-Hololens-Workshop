7bfacc44f6deb3260ab0631bc15a59cd9dac2eeb
nakisa <nakisa.donnelly@gmail.com>
2020-10-29 15:03:52 -0600

    Added Unity Project

---
+++ Hololens 2 Oct. 29 Workshop/Assets/Scripts/SpawnBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBall : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    public void SpawnBallPrefab()
+    {
+        Vector3 position = new Vector3(this.transform.position.x, this.transform.position.y + 0.2f, this.transform.position.z);
+
+        GameObject spawnBall = Instantiate(ballPrefab, position, Quaternion.identity);
+    }
+}
